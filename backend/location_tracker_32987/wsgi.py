3ef0ce3f3f1907c91df60cd34c08c2ec0a476766
Crowdbotics <team@crowdbotics.com>
2022-01-21 01:36:25 +0000

    Update models

--- backend/location_tracker_32987/wsgi.py
+++ backend/location_tracker_32987/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'location_tracker_32987.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "location_tracker_32987.settings")
 
 application = get_wsgi_application()
